dd74e755e63a1750fa48b5b596bbae2834782281
Zennswagg <99397372+Zennswagg@users.noreply.github.com>
2023-10-10 10:51:23 +0800

    Create firstjavaapp.java

---
+++ firstjavaapp.java
@@ -0,0 +1,50 @@
+/**
+ * @(#)firstjavaapp.java
+ *
+ * firstjavaapp application
+ *
+ * @author 
+ * @version 1.00 2023/9/14
+ */
+ 
+ //single-line comment
+ /*
+  *this is a multi-line comment
+  */
+  
+  /*Data types
+   *- int - whole number
+   *- double - number with decimal places 
+   *- float - number with decimal places (yes its the same as double)
+   *- string - sequence of characters. This --> ("")
+   *- char - single characters. this --> (')
+   *-  boolean - true or false
+  */
+  
+// this is the class named firstJavaApp
+public class firstjavaapp {
+    
+    // main method - main entry point of every java program
+    public static void main(String[] args) {
+    	
+        String name1 = " Nasa system plus haha";
+    	String name = " by:Renz Alfredo V. Gabun";
+        int age = 17;
+        String section = "MAWD11A" ;
+        
+        
+    	
+    	
+    	// this is an example of program statement
+    	//concatenation - combining strings
+        System.out.println("hi AJ Good Morning,"+ name);
+        System.out.println("You are "+ age + " years old.");
+        System.out.println("Your secstion is "+ section);
+        System.out.println("whos your crush,"+ name1);
+    	
+    	
+   
+   
+   
+    }
+}
